a9610fa3d732beb926ad11a29cd41d2e74deea8a
Hisham Alyahya <hisham.alyahya@student.manchester.ac.uk>
2021-08-25 23:51:11 +0100

    Add instruction filtering in TextCleaner

--- helper_functions/text_cleaning.py
+++ helper_functions/text_cleaning.py
@@ -2,20 +2,29 @@ import json
 import string
 import regex as re
 from nltk.stem import WordNetLemmatizer
-import nltk
-nltk.download('wordnet')
+from nltk.corpus import stopwords
+from nltk import download
+download('wordnet')
+download('stopwords')
+
 
 class TextCleaner:
-	def __init__(self, directory):
-		self.directory = directory
+	def __init__(self, directory=None):
+		"""
+			Parameters:
+				directory: path to food_names.json and synonyms.json (set to None if
+				you need to use without preloading)
+		"""
+		if directory:
+			self.directory = directory
 
-		with open(f'{directory}/food_names.json', 'r') as f:
-			self.food_names = json.load(f)
+			with open(f'{directory}/food_names.json', 'r') as f:
+				self.food_names = json.load(f)
 
-		with open(f'{directory}/synonyms.json', 'r') as f:
-			self.synonyms = json.load(f)
+			with open(f'{directory}/synonyms.json', 'r') as f:
+				self.synonyms = json.load(f)
 
-		self.all_names = set(self.food_names).union(self.synonyms.keys())
+			self.all_names = set(self.food_names).union(self.synonyms.keys())
 
 		self.lemmatizer = WordNetLemmatizer()
 
@@ -93,3 +102,57 @@ class TextCleaner:
 				ing += self.get_name(words[i])
 
 		return " ".join(ing.split())
+
+
+	def filter_instruction(self, ins: str):
+		ins = ins.lower()
+
+		ins = ins.replace('-', ' ')
+		ins = ins.replace(',', ' ')
+		ins = ins.replace('/', ' ')
+
+		# remove punctuation
+		ins = ins.translate(str.maketrans('', '', string.punctuation))
+
+		# remove digits
+		ins = re.sub(r'\d', "", ins)
+
+		# lemmatize words and remove stopwords
+		sw = set(stopwords.words('english'))
+		words = [self.lemmatizer.lemmatize(word) for word in ins.split() if word not in sw]
+
+		ins = ''
+		i = 0
+		while i < len(words) - 2:
+			if f'{words[i]}_{words[i+1]}_{words[i+2]}' in self.all_names:
+				ins += self.get_name(f'{words[i]}_{words[i+1]}_{words[i+2]}') + ' '
+				i += 2
+			elif f'{words[i]}_{words[i+1]}' in self.all_names:
+				ins += self.get_name(f'{words[i]}_{words[i+1]}') + ' '
+				i += 1
+			elif f'{words[i+1]}_{words[i]}' in self.all_names:
+				ins += self.get_name(f'{words[i+1]}_{words[i]}') + ' '
+				i += 1
+			elif words[i] in self.all_names:
+				ins += self.get_name(words[i]) + " "
+			else:
+				ins += words[i] + " "
+			i += 1
+
+		# if there are 2 remaining words
+		if i == len(words) - 2:
+			if f'{words[i]}_{words[i+1]}' in self.all_names:
+				ins += self.get_name(f'{words[i]}_{words[i+1]}')
+			elif f'{words[i+1]}_{words[i]}' in self.all_names:
+				ins += self.get_name(f'{words[i+1]}_{words[i]}')
+			elif f'{words[i+1]}_{words[i]}' in self.all_names:
+				ins += self.get_name(f'{words[i+1]}_{words[i]}')
+			else:
+				ins += " ".join(words[-2:])
+
+		# if there's a remaining word
+		if i == len(words) - 1:
+			if words[i] in self.all_names:
+				ins += self.get_name(words[i])
+
+		return " ".join(ins.split())
